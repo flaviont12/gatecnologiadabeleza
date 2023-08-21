b9a24a04c87719cfcd3620a0c191ea4dbd03467a
Flavio Neto <flaviont12@gmail.com>
2023-08-21 00:16:19 -0300

    configure links and change avatar

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ function toggleMode() {
   const img = document.querySelector('#profile img')
 
   // substituir a imagem
-  if (html.classList.contains('light')) {
-    // se tiver light mode add imagem light
-    // seleciona o atributo e aponta o novo valor
-    img.setAttribute('src', 'assets/avatar-light.png')
-  } else {
-    // se tiver sem light mode, manter a imagem normal
-    // seleciona o atributo e aponta o novo valor
-    img.setAttribute('src', 'assets/avatar.png')
-  }
+  // if (html.classList.contains('light')) {
+  //   // se tiver light mode add imagem light
+  //   // seleciona o atributo e aponta o novo valor
+  //   img.setAttribute('src', 'assets/avatar-light.png')
+  // } else {
+  //   // se tiver sem light mode, manter a imagem normal
+  //   // seleciona o atributo e aponta o novo valor
+  //   img.setAttribute('src', 'assets/avatar.png')
+  // }
 }
